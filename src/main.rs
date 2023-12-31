969f57cd0079a5f7190367f1f5bad9bf411ba462
PortWatcher <jury@skyquakers.com>
2024-01-01 01:34:15 +0900

    feat: async add_to_cache

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-mod s3;
 mod fs;
+mod s3;
 
+use crate::fs::{generate_file_path, save_stream_to_disk};
 use crate::s3::get_file_from_s3;
-use crate::fs::{save_stream_to_disk, generate_file_path};
 
-use rocket::{get, routes, main, State, http::Status, http::ContentType};
-use std::path::PathBuf;
-use dotenv::dotenv;
 use aws_sdk_s3::Client;
+use dotenv::dotenv;
 use lru::LruCache;
+use rocket::{get, http::ContentType, http::Status, main, routes, State};
 use std::num::NonZeroUsize;
-use std::sync::{Mutex, Arc};
-use std::fs::remove_file;
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 struct AppState {
     cache: Mutex<LruCache<String, (PathBuf, ContentType)>>,
@@ -19,22 +19,30 @@ struct AppState {
 }
 
 impl AppState {
-    fn add_to_cache(&self, key: String, file_path: PathBuf, content_type: ContentType) {
-        let mut cache = self.cache.lock().unwrap();
+    async fn add_to_cache(
+        &self,
+        key: String,
+        file_path: PathBuf,
+        content_type: ContentType,
+    ) -> Result<(), anyhow::Error> {
+        let mut cache = self.cache.lock().await;
 
         if cache.len() == cache.cap().get() {
             if let Some((_evicted_key, (evicted_file_path, _))) = cache.pop_lru() {
-                let _ = remove_file(&evicted_file_path);
+                tokio::fs::remove_file(&evicted_file_path).await?;
             }
         }
 
         cache.put(key, (file_path, content_type));
+        Ok(())
     }
 }
 
-
 #[get("/<path..>")]
-async fn index(path: PathBuf, state: &State<Arc<AppState>>) -> Result<(ContentType, Vec<u8>), Status> {
+async fn index(
+    path: PathBuf,
+    state: &State<Arc<AppState>>,
+) -> Result<(ContentType, Vec<u8>), Status> {
     let key = match path.into_os_string().into_string() {
         Ok(k) => k,
         Err(_) => return Err(Status::BadRequest),
@@ -42,17 +50,11 @@ async fn index(path: PathBuf, state: &State<Arc<AppState>>) -> Result<(ContentTy
 
     let s3key = key.replace("\\", "/");
     let app_state = state.inner();
-    
+
     {
-        let mut cache = match app_state.cache.lock() {
-            Ok(guard) => guard,
-            Err(poisoned) => {
-                eprintln!("Cache lock is poisoned. Using recovered state.");
-                poisoned.into_inner()
-            }
-        };
+        let mut cache = app_state.cache.lock().await;
         if let Some((file_path, content_type)) = cache.get(&s3key) {
-            if let Ok(data) = std::fs::read(&file_path) {
+            if let Ok(data) = tokio::fs::read(&file_path).await {
                 return Ok((content_type.clone(), data));
             }
         }
@@ -62,22 +64,24 @@ async fn index(path: PathBuf, state: &State<Arc<AppState>>) -> Result<(ContentTy
     match get_file_from_s3(&app_state.s3_client, &bucket, &s3key).await {
         Ok((byte_stream, content_type)) => {
             let file_path = generate_file_path(&s3key);
-            
+
             // Save the stream to disk
             if let Err(e) = save_stream_to_disk(&file_path, byte_stream).await {
                 eprintln!("Failed to save file: {}", e);
                 return Err(Status::InternalServerError);
             }
-            
+
             // Add to cache and read the file to send to the client
-            app_state.add_to_cache(s3key.clone(), file_path.clone(), content_type.clone());
+            app_state
+                .add_to_cache(s3key.clone(), file_path.clone(), content_type.clone())
+                .await
+                .expect("add to cache failed");
             let data = std::fs::read(&file_path).expect("Failed to read file");
             Ok((content_type, data))
-        },
+        }
         Err(_) => Err(Status::NotFound),
     }
 }
-
 
 #[main]
 async fn main() -> Result<(), rocket::Error> {
