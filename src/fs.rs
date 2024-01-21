d2bddc9efa51ec90406b2c41e70c71faef5fbaea
PortWatcher <jury@skyquakers.com>
2024-01-21 20:50:56 +0900

    chore: remove debug log

--- src/fs.rs
+++ src/fs.rs
@@ -35,7 +35,6 @@ pub fn generate_key_from_filename(filename: &str) -> String {
 
 pub fn is_key_cached(key: &str) -> bool {
     let path = generate_file_path(key);
-    println!("checking if file exists: {:?}", path);
     path.exists()
 }
 
